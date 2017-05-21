2c6f53a288d077a6ff5f104c5b4260191f3a0405
SeHyeon Yang <SeHyeon Yang>
2017-05-21 19:54:58 +0900

    sign up in

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.welcome, name='welcome'),
     url(r'^welcome/$', views.welcome, name='welcome'),
+    url(r'^sign_in/$', views.sign_in, name='sign_in'),
+    url(r'^sign_up/$', views.sign_up, name='sign_up'),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def welcome(request):
     return render(request, 'app/class_list.html', {})
+
+
+def sign_in(request):
+    return render(request, 'app/sign_in.html', {})
+
+
+def sign_up(request):
+    return render(request, 'app/sign_up.html', {})
+
