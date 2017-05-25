880499aded6197d83cfdce790237f7c1093828fb
SeHyeon Yang <SeHyeon Yang>
2017-05-25 18:44:39 +0900

    commit

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^welcome/$', views.welcome, name='welcome'),
     url(r'^sign_in/$', views.sign_in, name='sign_in'),
     url(r'^sign_up/$', views.sign_up, name='sign_up'),
+
 ]
