25359816ff37817c49d0e863a8e067e0de44a2fb
eonji <djswl2624@naver.com>
2017-05-31 14:39:20 +0900

    add urls

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^welcome/$', views.welcome, name='welcome'),
     url(r'^sign_in/$', views.sign_in, name='sign_in'),
     url(r'^sign_up/$', views.SignUp.as_view(), name='sign_up'),
+    url(r'^sign_up/$', views.SignUp.as_view(), name='group'),
 
 ]
