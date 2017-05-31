fc62dce1502623b5d657852c18d8f2811ad561f3
SeHyeon Yang <SeHyeon Yang>
2017-05-31 17:49:01 +0900

    my_page pages

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     url(r'^$', views.welcome, name='welcome'),
     url(r'^welcome/$', views.welcome, name='welcome'),
     url(r'^sign_in/$', views.sign_in, name='sign_in'),
-    url(r'^lecture/add$', views.lecture_add, name='lecture_add'),
-    url(r'^sign_up/$', views.SignUp.as_view(), name='group'),
+    url(r'^lecture/add/$', views.lecture_add, name='lecture_add'),
+    url(r'^sign_up/$', views.SignUp.as_view(), name='sign_up'),
     url(r'^id_check/$', views.id_check, name='id_check'),
-    url(r'^my_page/$', views.my_page, name='my_page'),
+    url(r'^my_page/(?P<menu>[\w]+)/$', views.my_page, name='my_page'),
 ]
