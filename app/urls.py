2a9080f065ef37894b380b2fb1f7248e178fe4c8
SeHyeon Yang <SeHyeon Yang>
2017-05-31 15:59:51 +0900

    my_page start

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^sign_in/$', views.sign_in, name='sign_in'),
     url(r'^sign_up/$', views.SignUp.as_view(), name='sign_up'),
     url(r'^id_check/$', views.id_check, name='id_check'),
+    url(r'^my_page/$', views.my_page, name='my_page'),
 ]
