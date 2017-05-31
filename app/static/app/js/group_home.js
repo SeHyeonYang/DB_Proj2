8e7f8e6dd8668476ea6ba0230547258a78e0e8de
eonji <djswl2624@naver.com>
2017-05-31 18:21:23 +0900

    1

---
+++ app/static/app/js/group_home.js
@@ -0,0 +1,19 @@
+/**
+ * Created by user on 2017-05-31.
+ */
+angular.module('AniTheme').controller('DropdownCtrl', function ($scope, $log) {
+    $scope.status = {
+        isopen: false
+    };
+
+    $scope.toggled = function(open) {
+        $log.log('Dropdown is now: ', open);
+    };
+
+    $scope.toggleDropdown = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+        $scope.status.isopen = !$scope.status.isopen;
+    };
+});
+
