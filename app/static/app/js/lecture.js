6d82167af1dff73345388df5f2ead02f25f18b79
YeLim Han <YeLim Han>
2017-06-07 00:30:55 +0900

    lecture add

--- app/static/app/js/lecture.js
+++ app/static/app/js/lecture.js
@@ -11,7 +11,7 @@ function lecture_distinct_check(_lecture_input_id) {
     });
 
     $.ajax({
-        url: '/app/id_check/?data=' + lecture_id + '/',
+        url: '/app/lecture_check/?data=' + lecture_id + '/',
         type: 'POST',
         success: function (result) {
             var dict = JSON.parse(result);
@@ -29,4 +29,20 @@ function lecture_distinct_check(_lecture_input_id) {
         }
     });
 
+}
+
+function get_cookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
--- app/views/lecture.py
+++ app/views/lecture.py
@@ -3,11 +3,14 @@ from django.views.generic import View
 from django.http import HttpResponseRedirect, HttpResponse
 from app.models import *
 
-
+import json
 
 
 def lecture_all(request):
     return render(request, 'app/lecture_all.html', {})
+
+def lecture_category(request):
+    return render(request, '', {})
 
 class LectureAdd(View):  # 강좌 개설
     def get(self, request):
@@ -25,6 +28,28 @@ class LectureAdd(View):  # 강좌 개설
         data = request.POST
         lecture_select = data['lecture_select']
         lecture_name = data['lecture_name']
+        lecture_describe = data['lecture_describe']
+
+        lecture = Course.objects.created(title=lecture_name, category_id=lecture_select, comments=lecture_describe)
+        lecture.save()
+
+        return HttpResponseRedirect('/app/lecture/all/')
+
+
+class SectionAdd(View):
+    def get(self, request):
+        category_list = Category.objects.all()
+
+        for category in category_list:
+            temp_dict = dict()
+            temp_dict['category_id'] = category.id
+            temp_dict['category_name'] = category.category_name
+        context = {}
+        context['category_list'] = category_list
+        return render(request, 'app/lecture_section_add.html', context)
+
+    def post(self, request):
+        data = request.POST
         lecture_sum = data['lecture_sum']
         lecture_min_pnum = data['lecture_min_pnum']
         lecture_max_pnum = data['lecture_max_pnum']
@@ -38,7 +63,11 @@ class LectureAdd(View):  # 강좌 개설
         sunday_button = data['sunday_button']
         lecture_start_time = data['lecture_start_time']
         lecture_finish_time = data['lecture_finish_time']
-        lecture_describe = data['lecture_describe']
 
 
-        return HttpResponseRedirect('/app/lecture/all')
+def lecture_check(request):
+    lecture_name = request.GET.get('data')[:-1]
+    check = Course.objects.filter(title=lecture_name).count()
+    temp = dict()
+    temp['distinct_check'] = check
+    return HttpResponse(json.dumps(temp))
