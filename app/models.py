16b7ee2cf4219a0a31594e3589b9d0236128588b
SeHyeon Yang <SeHyeon Yang>
2017-06-08 03:00:40 +0900

    db

--- app/models.py
+++ app/models.py
@@ -19,7 +19,7 @@ class AppUser(models.Model):
     name = models.CharField(max_length=45)
     nickname = models.CharField(max_length=45)
 
-    
+
 def default_time():
     now = datetime.now()
     start = now.replace(hour=22, minute=0, second=0, microsecond=0)
@@ -98,11 +98,6 @@ class Group(models.Model):
     category = models.ForeignKey(Category)
 
 
-class UserGroup(models.Model):
-    user_id = models.ForeignKey(User)
-    group_id = models.ForeignKey(Group)
-
-
 class Article(models.Model):
     # article_id is auto serial key. 1,2,3,4,...
     user_id = models.ForeignKey(User)
@@ -112,4 +107,11 @@ class Article(models.Model):
     notice = models.BooleanField(default=False)
 
 
+class UserGroup(models.Model):
+    user_id = models.ForeignKey(User)
+    group_id = models.ForeignKey(Group)
 
+
+class GroupArticle(models.Model):
+    article_id = models.ForeignKey(Article)
+    group_id = models.ForeignKey(Group)
